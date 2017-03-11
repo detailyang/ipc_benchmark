5ccb435e6659d76bd61f36c83461ab33e4a0581d
detailyang <detailyang@gmail.com>
2017-03-12 00:37:12 +0800

    tests: add tests

---
+++ Makefile
@@ -0,0 +1,24 @@
+CC := gcc
+CFLAGS := -g -Wall
+TARGET := pipe fifo socketpair uds tcp
+
+all: $(TARGET)
+
+pipe: pipe.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+fifo: fifo.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+socketpair: socketpair.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+uds: uds.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+tcp: tcp.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+.PHONY: test
+test:
+	./run_tests.sh
---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+readonly IPCS=("pipe" "fifo" "socketpair" "uds" "tcp")
+readonly SIZE=("128" "512" "1024" "4096")
+
+for ipc in ${IPCS[@]}; do
+    result=()
+    for i in ${SIZE[@]}; do
+    result+=($i)
+        t=$(./$ipc $i 1024)
+    result+=($t)
+    done
+
+    printf "%s\n" $ipc
+    for value in ${result[@]}; do
+    printf "%s\n" $value
+    done | column
+    echo ""
+done
